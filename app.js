3c39535d85bc5728e9bd34bc0e55834bcd013121
Vaschi <bnilodan@gmail.com>
2024-03-05 18:23:21 +0100

    Initial commit

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 /////// THE SETUP
 
-const gravity = 0.2; // acceleration
+const gravity = 0.7; // acceleration
 
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity; // gravity, physics
     this.height = 150; // the bottom of the rect
+    this.lastKey;
   }
   draw() {
     c.fillStyle = "red";
@@ -23,8 +24,10 @@ class Sprite {
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
-    // make it stop at the botom
+
+    // make it stop at the bottom
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
       this.velocity.y = 0;
     } else {
@@ -33,6 +36,22 @@ class Sprite {
   }
 }
 
+// last key pressed movement
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+  ArrowLeft: {
+    pressed: false,
+  },
+};
+let lastKey;
 ////// GAME ASSETS, WHAT YOU SEE ////////
 
 const player = new Sprite({
@@ -59,14 +78,81 @@ const enemy = new Sprite({
 player.draw();
 enemy.draw();
 
-/////////////////// GAMEPLAY, GIVE IT LIFE
+/////////////////// GAMEPLAY, GIVE IT LIFE ANIMATE
 
 // infinite loop
 function animate() {
   window.requestAnimationFrame(animate);
+
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
+
+  // Player
+  player.velocity.x = 0;
+  if (keys.a.pressed && player.lastKey === "a") {
+    player.velocity.x = -5;
+  } else if (keys.d.pressed && player.lastKey === "d") {
+    player.velocity.x = 5;
+  }
+
+  // Enemy
+  enemy.velocity.x = 0;
+  if (keys.ArrowLeft.pressed && enemy.lastKey === "ArrowLeft") {
+    enemy.velocity.x = -5;
+  } else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
+    enemy.velocity.x = 5;
+  }
 }
+
+// Movement
+window.addEventListener("keydown", (e) => {
+  // Player
+  switch (e.key) {
+    case "d":
+      keys.d.pressed = true;
+      player.lastKey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      player.lastKey = "a";
+      break;
+    case "w":
+      player.velocity.y = -20;
+      break;
+    // Enemy
+
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;
+      enemy.lastKey = "ArrowRight";
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      enemy.lastKey = "ArrowLeft";
+      break;
+    case "ArrowUp":
+      enemy.velocity.y = -20;
+      break;
+  }
+});
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    // Player
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    // Enemy
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+      break;
+  }
+});
+
 animate();
